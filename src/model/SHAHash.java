82ee5a9794e291bf0478e084d357859262c0dc13
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-04-25 22:30:46 +1000

    Introduced SHAHash class to calculate SHA256 hashes

---
+++ src/model/SHAHash.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHAHash {
+    public String getHash(String toHash) throws NoSuchAlgorithmException {
+        //Referenced code from [1]
+        MessageDigest MD = MessageDigest.getInstance("SHA-256");
+        MD.update(toHash.getBytes());
+
+        byte[] digest = MD.digest();
+        StringBuffer StringBuffer = new StringBuffer();
+        for (byte b : digest){
+            StringBuffer.append(String.format("%02x", b & 0xff));
+        }
+        return StringBuffer.toString();
+    }
+}
