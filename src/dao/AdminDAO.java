1426a3bbfc4592b7ffab4ea8c7521227a108539f
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-04-26 15:21:49 +1000

    Added methods to delete employee, update account and add admin

---
+++ src/dao/AdminDAO.java
@@ -0,0 +1,106 @@
+package dao;
+
+import model.EmployeeModel;
+import model.SHAHash;
+
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminDAO extends EmployeeDAO{
+    private Connection connect = SQLConnection.connect();
+    private List<EmployeeModel> listOfEmployees = new ArrayList<>();
+    SHAHash HASH = new SHAHash();
+
+    //delete an account using the account's id
+    public boolean deleteAccount(int id){
+        boolean result = true;
+        String sql = "DELETE FROM Employee WHERE id = ?";
+        try{
+            PreparedStatement pstmt = connect.prepareStatement(sql);{
+                pstmt.setInt(1, id);
+                pstmt.executeUpdate();
+                updateEmployee();
+            }
+            for(EmployeeModel Emp : listOfEmployees){
+                if(Emp.getID() == id){
+                    result = false;
+                    break;
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    //allows admin to update an account
+    public boolean updateAccount (int CurrentID, int id, String Firstname, String Lastname, String Username, String Password,
+                                  String Secret_Question, String SQ_Answer){
+        boolean result = false;
+        String sql = "UPDATE Employee SET id = ?, Firstname = ?, Lastname = ?, Username = ?, Password = ?, " +
+                "Secret_Question = ?, SQ_Answer = ? WHERE id = ?";
+        try{
+            PreparedStatement pstmt = connect.prepareStatement(sql);{
+                pstmt.setInt(1, id);
+                pstmt.setString(2, Firstname);
+                pstmt.setString(3, Lastname);
+                pstmt.setString(4, Username);
+                pstmt.setString(5, HASH.getHash(Password));
+                pstmt.setString(6, Secret_Question);
+                pstmt.setString(7, HASH.getHash(SQ_Answer));
+                pstmt.setInt(8, CurrentID);
+                pstmt.executeUpdate();
+            }
+            for(EmployeeModel Emp : listOfEmployees){ //check the account ID is valid
+                if(Emp.getID() == id){
+                    result = true;
+                    break;
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    //Adding an admin account
+    public boolean addAccount(int id, String Firstname, String Lastname, String Username, String Password,
+                              String Secret_Question, String SQ_Answer){
+        boolean add = false;
+        String sql = "INSERT INTO Employee VALUES (?,?,?,?,?,?,?,?)";
+
+        try{
+            PreparedStatement pstmt = connect.prepareStatement(sql);{
+                pstmt.setInt(1, id);
+                pstmt.setString(2, Firstname);
+                pstmt.setString(3, Lastname);
+                pstmt.setString(4, Username);
+                pstmt.setString(5, HASH.getHash(Password));
+                pstmt.setString(6, "admin");
+                pstmt.setString(7, Secret_Question);
+                pstmt.setString(8, HASH.getHash(SQ_Answer));
+                pstmt.executeUpdate();
+                updateEmployee();
+            }
+            for(EmployeeModel Emp : listOfEmployees){
+                if(Emp.getID() == id){
+                    add = true;
+                    break;
+                }
+            }
+        }catch (SQLException | NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        return add;
+    }
+
+
+
+
+
+}
