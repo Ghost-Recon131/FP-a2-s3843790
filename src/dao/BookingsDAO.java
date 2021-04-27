01c575295b7b4684ca8367e675ed7f9496bbf5ff
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-04-27 10:42:19 +1000

    Progress on BookingsDAO

--- src/dao/BookingsDAO.java
+++ src/dao/BookingsDAO.java
@@ -1,15 +1,28 @@
 package dao;
 import model.BookingsModel;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookingsDAO {
     private Connection connect = SQLConnection.connect();
     private List<BookingsModel> listOfBookings = new ArrayList<>();
+
+    //refresh the database
+    public void updateBookings() {
+        try {
+            Statement myStmt = connect.createStatement();
+            ResultSet RS = myStmt.executeQuery("select * from Bookings");
+            while (RS.next()) {
+                listOfBookings.add(new BookingsModel(RS.getInt("id"), RS.getInt("BookingID"),
+                        RS.getString("Role"), RS.getInt("TableNumber"),
+                        RS.getInt("PreviousTableNumber"), RS.getString("BookingDate"),
+                        RS.getString("SittingDate"), RS.getString("TableStatus")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
