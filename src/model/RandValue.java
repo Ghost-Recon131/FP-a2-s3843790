7f68c33754779fbcd67d3e23a3be3fe3ed25efc5
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-04-28 21:30:14 +1000

    Moved random number generation to its own java model

---
+++ src/model/RandValue.java
@@ -0,0 +1,46 @@
+package model;
+
+import dao.BookingsDAO;
+import dao.EmployeeDAO;
+import java.util.Random;
+
+public class RandValue {
+
+    public int randomID(){
+        EmployeeDAO EDAO = new EmployeeDAO();
+        Random R = new Random();
+        int random = R.nextInt(10000);
+        int backup = R.nextInt(10000);
+        int returnvalue;
+
+        if(random != EDAO.getAccountID(random)){ //getAccountID will return -1 if there are no matches
+            returnvalue = random;
+        }
+        else if(random != EDAO.getAccountID(random)){
+            returnvalue = backup;
+        }
+        else{ // in the unlikely event of a match, we will introduce another random variable
+            returnvalue = random + backup;
+        }
+        return returnvalue;
+    }
+
+    public int randomBookingID(){
+        BookingsDAO BDAO = new BookingsDAO();
+        Random R = new Random();
+        int random = R.nextInt(100000);
+        int backup = R.nextInt(100000);
+        int returnvalue;
+
+        if(random != BDAO.getBookingID(random)){ //getAccountID will return -1 if there are no matches
+            returnvalue = random;
+        }
+        else if(random != BDAO.getBookingID(random)){
+            returnvalue = backup;
+        }
+        else{ // in the unlikely event of a match, we will introduce another random variable
+            returnvalue = random + backup;
+        }
+        return returnvalue;
+    }
+}
--- src/model/SHAHash.java
+++ src/model/SHAHash.java
@@ -1,10 +1,8 @@
 package model;
 
-import dao.EmployeeDAO;
-
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Random;
+
 
 public class SHAHash {
     public String getHash(String toHash) throws NoSuchAlgorithmException {
@@ -20,22 +18,4 @@ public class SHAHash {
         return StringBuffer.toString();
     }
 
-    public int Randomint(){
-        EmployeeDAO EDAO = new EmployeeDAO();
-        Random R = new Random();
-        int random = R.nextInt(10000);
-        int backup = R.nextInt(10000);
-        int returnvalue;
-
-        if(random != EDAO.getAccountID(random)){ //getAccountID will return -1 if there are no matches
-            returnvalue = random;
-        }
-        else if(random != EDAO.getAccountID(random)){
-            returnvalue = backup;
-        }
-        else{ // in the unlikely event of a match, we will introduce another random variable
-            returnvalue = random + backup;
-        }
-        return returnvalue;
-    }
 }
