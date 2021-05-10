a6f53115e266a34a9fb4993fdce9e954d32a89a1
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-10 12:31:10 +1000

    completed ResetPasswordModel

--- src/model/ResetPasswordModel.java
+++ src/model/ResetPasswordModel.java
@@ -1,8 +1,11 @@
 package model;
 
+import controller.utils.RandPasswordUtil;
 import controller.utils.SHAHashUtil;
+import dao.EmployeeDAO;
 import dao.SQLConnection;
 
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,16 +14,18 @@ import java.sql.SQLException;
 public class ResetPasswordModel {
     private Connection connect = SQLConnection.connect();
     SHAHashUtil HASH = new SHAHashUtil();
+    RandPasswordUtil RPU = new RandPasswordUtil();
+    EmployeeDAO EDAO = new EmployeeDAO();
 
     public Boolean isLogin(String SQ, String SQ_A) throws SQLException {
-        PreparedStatement preparedStatement = null;
+        PreparedStatement pstmt = null;
         ResultSet resultSet=null;
         String query = "select * from employee where Secret_Question = ? and SQ_Answer= ?";
         try {
-            preparedStatement = connect.prepareStatement(query);
-            preparedStatement.setString(1, SQ);
-            preparedStatement.setString(2, HASH.getHash(SQ_A));
-            resultSet = preparedStatement.executeQuery();
+            pstmt = connect.prepareStatement(query);
+            pstmt.setString(1, SQ);
+            pstmt.setString(2, HASH.getHash(SQ_A));
+            resultSet = pstmt.executeQuery();
             if (resultSet.next()) {
                 return true;
             }
@@ -28,13 +33,18 @@ public class ResetPasswordModel {
                 return false;
             }
         }
-        catch (Exception e)
-        {
+        catch (Exception e){
             return false;
         }finally {
-            preparedStatement.close();
+            pstmt.close();
             resultSet.close();
         }
+    }
+
+    public String ResetPassword(String SQ_A) throws NoSuchAlgorithmException {
+        String newPassword = RPU.getRandomPassword();
+        EDAO.resetPassword(SQ_A, newPassword);
+        return newPassword;
     }
 
 }
