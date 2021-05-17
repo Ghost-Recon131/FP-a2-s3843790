4165d5778fc110838588b528c646005173959d95
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-17 21:27:42 +1000

    Finished preliminary design of TableDAO

--- src/dao/TableDAO.java
+++ src/dao/TableDAO.java
@@ -1,11 +1,8 @@
 package dao;
 
+import model.LoginModel;
 import model.TableModel;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,10 +24,65 @@ public class TableDAO {
         }
     }
     //todo complete TableDAO
+    //get the status of a table via table number
+    public int getTableStatus(int TableNumber) {
+        int TableStatus = -1;
+        for (TableModel Tbm : listOfTables) {
+            if (Tbm.getTableNumber() == TableNumber) {
+                TableStatus = Tbm.getTableStatus();
+            }
+        }
+        return TableStatus;
+    }
 
+    public boolean getCOVID(int TableNumber) {
+        boolean COVIDLockdown = true;
+        for (TableModel Tbm : listOfTables) {
+            if (Tbm.getTableNumber() == TableNumber) {
+                if(Tbm.getCOVID() == 0){
+                    COVIDLockdown = false;
+                }
+            }
+        }
+        return COVIDLockdown;
+    }
 
+    // get the admin's message about a table
+    public String getAdminMessage(int TableNumber) {
+        String message = null;
+        for (TableModel Tbm : listOfTables) {
+            if (Tbm.getTableNumber() == TableNumber) {
+                message = Tbm.getAdminMessage();
+            }
+        }
+        return message;
+    }
 
-
-
+    // allows for setting custom messages
+    // table -1 is reserved for an announcement that applies to all users & is not able to be reserved for seating
+    public boolean UpdateAdminMessage(String newMessage, int TableNumber) {
+        boolean update = false;
+        if(LoginModel.getCurrentUserRole().equals("admin")){
+            String sql = "UPDATE Tables SET AdminMessage = ? WHERE TableNumber = ?";
+            try {
+                PreparedStatement pstmt = connect.prepareStatement(sql);
+                {
+                    pstmt.setString(1, newMessage);
+                    pstmt.setInt(2, TableNumber);
+                    pstmt.executeUpdate();
+                    updateTables();
+                }
+                for (TableModel Tbm : listOfTables) {
+                    if (Tbm.getTableNumber() == TableNumber) {
+                        update = true;
+                        break;
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return update;
+    }
 
 }
