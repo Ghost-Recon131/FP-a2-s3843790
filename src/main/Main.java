84a050c8bd79ec748535fbdc9c02948d558fa960
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-17 12:32:00 +1000

    added foreign keys to database

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        //launch(args);
+        launch(args);
         //temporary code for testing
     }
 }
