f52bcc37656146be3c275627f28be59a222c115d
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-17 16:39:12 +1000

    progress on BookingsDAO

--- src/dao/BookingsDAO.java
+++ src/dao/BookingsDAO.java
@@ -1,7 +1,9 @@
 package dao;
+import controller.utils.RandValueUtil;
 import model.BookingsModel;
 import model.EmployeeModel;
 
+import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +11,7 @@ import java.util.List;
 public class BookingsDAO {
     private Connection connect = SQLConnection.connect();
     private List<BookingsModel> listOfBookings = new ArrayList<>();
+    RandValueUtil RV = new RandValueUtil();
 
     //refresh the database
     public void updateBookings() {
@@ -37,15 +40,101 @@ public class BookingsDAO {
         return ID;
     }
 
-    //get the booking ID via booking ID
-    public int getBookingID(int BID) {
+    //get the booking ID via user ID
+    public int getBookingID(int ID) {
         int bookingID = -1;
         for (BookingsModel Bkm : listOfBookings) {
-            if (Bkm.getBookingID() == BID){
+            if (Bkm.getID() == ID){
                 bookingID = Bkm.getBookingID();
             }
         }
         return bookingID;
+    }
+
+    //get booking status via BookingID
+    public String getBookingStatus(int BID){
+        String BookingStatus = null;
+        for (BookingsModel Bkm : listOfBookings){
+            if (Bkm.getBookingID() == BID){
+                BookingStatus = Bkm.getBookingStatus();
+            }
+        }
+        return BookingStatus;
+    }
+
+    //get Table Number via BookingID
+    public int getTableNumber(int BID){
+        int TableNumber = -1;
+        for (BookingsModel Bkm : listOfBookings){
+            if (Bkm.getBookingID() == BID){
+                TableNumber = Bkm.getTableNumber();
+            }
+        }
+        return TableNumber;
+    }
+
+    //get previous Table Number via BookingID
+    public int getPreviousTableNumber(int BID){
+        int PreviousTableNumber = -1;
+        for (BookingsModel Bkm : listOfBookings){
+            if (Bkm.getBookingID() == BID){
+                PreviousTableNumber = Bkm.getPreviousTableNumber();
+            }
+        }
+        return PreviousTableNumber;
+    }
+
+    //get Booking date via BookingID
+    public String getBookingDate(int BID){
+        String BookingDate = null;
+        for (BookingsModel Bkm : listOfBookings){
+            if (Bkm.getBookingID() == BID){
+                BookingDate = Bkm.getBookingDate();
+            }
+        }
+        return BookingDate;
+    }
+
+    //get setting date via BookingID
+    public String getSittingDate(int BID){
+        String SittingDate = null;
+        for (BookingsModel Bkm : listOfBookings){
+            if (Bkm.getBookingID() == BID){
+                SittingDate = Bkm.getSittingDate();
+            }
+        }
+        return SittingDate;
+    }
+
+    //Adding a booking
+    public boolean addBooking(int ID, String BookingStatus, int TableNumber, int PreviousTableNumber,
+                              String Date, String SittingDate){
+        boolean add = false;
+        String sql = "INSERT INTO Bookings VALUES (?,?,?,?,?,?,?)";
+        int BookingID = RV.randomBookingID(); //generates random ID to avoid accidentally assigning an ID that is already in use
+
+        try{
+            PreparedStatement pstmt = connect.prepareStatement(sql);{
+                pstmt.setInt(1, ID);
+                pstmt.setString(2, BookingStatus);
+                pstmt.setInt(3, TableNumber);
+                pstmt.setInt(4, PreviousTableNumber);
+                pstmt.setString(5, Date);
+                pstmt.setString(6, SittingDate); //only admin can add another admin
+                pstmt.setInt(7, BookingID);
+                pstmt.executeUpdate();
+                updateBookings();
+            }
+            for (BookingsModel Bkm : listOfBookings) {
+                if (Bkm.getBookingID() == ID){
+                    add = true;
+                    break;
+                }
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return add;
     }
 
     //todo finish implementing BookingsDAO
