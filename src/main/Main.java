b91949b4307d15cfa85aa28edf0cd21969ecf8aa
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-19 15:54:24 +1000

    added RegisterModel - yet to implement

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,9 @@
 package main;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.Objects;
 
@@ -24,7 +22,7 @@ public class Main extends Application {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         launch(args);
         //temporary code for testing
     }
