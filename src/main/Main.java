3e8d6dad2e42a42b7ccc40c66fe7f94885ff454b
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-19 15:38:56 +1000

    minor change

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,21 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../view/HomeScreen.fxml")));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
         }
-        catch (Exception e){
-            System.err.println(e);
-            System.err.println("Error in launching GUI");
+        catch (IOException e){
+            System.err.println("Error in  GUI");
         }
     }
 
