742be93b594217b54c56bb57d322d75dfa991937
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-19 14:12:53 +1000

    Start work on new fxml for home screen

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,10 +11,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../view/Login.fxml")));
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../view/HomeScreen.fxml")));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch (Exception e){
+            System.err.println("Error in launching GUI");
+        }
     }
 
     public static void main(String[] args) {
