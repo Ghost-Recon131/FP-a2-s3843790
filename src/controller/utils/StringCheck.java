140370748c95037cbc1ded2cf0dfac03b282aa67
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-19 20:42:24 +1000

    shortened method

--- src/controller/utils/StringCheck.java
+++ src/controller/utils/StringCheck.java
@@ -3,11 +3,7 @@ package controller.utils;
 public class StringCheck {
 
     public boolean VerifyString(String value){
-        boolean check = false;
-        if(value != null && value.length() > 0){
-            check = true;
-        }
-        return check;
+        return value != null && value.length() > 0;
     }
 
 }
