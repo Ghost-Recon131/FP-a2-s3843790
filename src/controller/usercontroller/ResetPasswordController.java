aa7fa2b2776a99d3519035b4a1487656e0cc3af9
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-20 17:46:38 +1000

    Bug fixes & almost complete feature

--- src/controller/usercontroller/ResetPasswordController.java
+++ src/controller/usercontroller/ResetPasswordController.java
@@ -4,31 +4,23 @@ import controller.HomeScreenController;
 import controller.utils.StringCheck;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import javafx.stage.Window;
 import model.usermodel.ResetPasswordModel;
-
-import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Objects;
 
 public class ResetPasswordController {
     @FXML private TextField SQ;
     @FXML private TextField SQ_A;
 
     @FXML private Label SQError;
+    @FXML private Label SQAError;
     @FXML private Label ErrorMessage;
 
     HomeScreenController HSC = new HomeScreenController();
     StringCheck StringCheck = new StringCheck();
     ResetPasswordModel ResetPasswordModel = new ResetPasswordModel();
-    FinalResetPasswordController FRPC = new FinalResetPasswordController();
 
     @FXML
     private Button HomeButton;
@@ -38,14 +30,13 @@ public class ResetPasswordController {
 
     // button activates the reset password process
     @FXML
-    private Button ResetPasswordButton;
+    public Button ResetPasswordButton;
     public void setResetPasswordButtonClick(ActionEvent event) {
         try {
             ResetPassword();
-        } catch (SQLException e){
-            System.err.println("Exception in reset password");
+        } catch (SQLException e) {
+            System.err.println("Exception occurred in password reset");
         }
-
     }
 
     // program logic for checking and resetting password
@@ -53,24 +44,25 @@ public class ResetPasswordController {
         boolean error1, error2, error3;
 
         // check that the entered information matches database
-        if (ResetPasswordModel.LocateUser(SQ.getText(), SQ_A.getText())){
+        if (!ResetPasswordModel.LocateUser(SQ.getText().toUpperCase(), SQ_A.getText())){
             SQError.setText("Secret question or answer is wrong");
             ErrorMessage.setText("Failed to reset password");
             error3 = true;
-        } else{
+        }else{
+            SQError.setText("");
+            ErrorMessage.setText("");
             error3 = false;
         }
 
         error1 = StringCheck.InputNotEmpty(SQ, SQError);
-        error2 = StringCheck.InputNotEmpty(SQ_A, SQError);
+        error2 = StringCheck.InputNotEmpty(SQ_A, SQAError);
 
         // proceed when there's no errors
         if (!error1 && !error2 && !error3){
             ResetPasswordModel.ResetPassword(SQ_A.getText());
-            FRPC.ResetPasswordScene(ResetPasswordButton);
+            //todo new method for showing password
         }
-
     }
-
+//todo update fxml to show password
 
 }
--- src/model/usermodel/ResetPasswordModel.java
+++ src/model/usermodel/ResetPasswordModel.java
@@ -13,33 +13,35 @@ import java.sql.SQLException;
 
 public class ResetPasswordModel {
     private Connection connect = SQLConnection.connect();
+    PreparedStatement pstmt = null;
+    ResultSet RS = null;
+
     private String NewPassword;
     SHAHashUtil HASH = new SHAHashUtil();
     RandPasswordUtil RPU = new RandPasswordUtil();
     EmployeeDAO EDAO = new EmployeeDAO();
 
-    public Boolean LocateUser(String SQ, String SQ_A) throws SQLException {
-        PreparedStatement pstmt = null;
-        ResultSet resultSet=null;
+    public boolean LocateUser(String SQ, String SQ_A) throws SQLException {
+        boolean Located;
         String query = "select * from employee where Secret_Question = ? and SQ_Answer = ?";
+
         try {
             pstmt = connect.prepareStatement(query);
             pstmt.setString(1, SQ);
             pstmt.setString(2, HASH.getHash(SQ_A));
-            resultSet = pstmt.executeQuery();
-            if (resultSet.next()) {
-                return true;
+            RS = pstmt.executeQuery();
+            if (RS.next()) {
+                Located = true;
+            }else{
+                Located = false;
             }
-            else{
-                return false;
-            }
-        }
-        catch (Exception e){
+        } catch (NoSuchAlgorithmException | SQLException e){
             return false;
         }finally {
             pstmt.close();
-            resultSet.close();
+            RS.close();
         }
+        return Located;
     }
 
     public boolean ResetPassword(String SQ_A) {
