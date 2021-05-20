8be774c76423b7c1a4bb8adfbcfecda5ee2f2330
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-20 14:26:38 +1000

    Moved InputNotEmpty method into this utility class

--- src/controller/utils/StringCheck.java
+++ src/controller/utils/StringCheck.java
@@ -1,9 +1,24 @@
 package controller.utils;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class StringCheck {
 
     public boolean VerifyString(String value){
         return value != null && value.length() > 0;
+    }
+
+    // sets error messages when information is missing
+    public boolean InputNotEmpty(TextField Variable, Label ErrorVariable){
+        boolean Error = true;
+        if(!VerifyString(Variable.getText())){
+            ErrorVariable.setText("This cannot be empty");
+        }
+        else{
+            Error = false;
+        }
+        return Error;
     }
 
 }
