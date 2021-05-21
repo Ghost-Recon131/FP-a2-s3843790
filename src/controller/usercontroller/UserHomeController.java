4e6dc02f81cdba8e901bf09fdaefed6e905a754a
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-21 13:53:58 +1000

    progress on User Home screen

--- src/controller/usercontroller/UserHomeController.java
+++ src/controller/usercontroller/UserHomeController.java
@@ -1,24 +1,36 @@
 package controller.usercontroller;
 
+import controller.HomeScreenController;
 import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.Window;
-
+import model.usermodel.UserHomeModel;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class UserHomeController implements Initializable {
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+public class UserHomeController implements Initializable{
+    @FXML private TextField COVIDNotifications;
+    @FXML private TextField GeneralNotifications;
+    @FXML private Label CheckinError;
+    @FXML private Label EmployeeName;
+    HomeScreenController HSC = new HomeScreenController();
+    UserHomeModel UHM = new UserHomeModel();
 
+    @Override // loads in some values as soon as user gets to home page
+    public void initialize(URL url, ResourceBundle rb) {
+        EmployeeName.setText(UHM.getEmployeeName());
+        COVIDNotifications.setText(UHM.getCOVIDNotification() );
+//        + "\n" + UHM.GetGlobalAdminMessage()
     }
 
     // allow other classes to switch back to UserHome scene
@@ -32,9 +44,13 @@ public class UserHomeController implements Initializable {
             primaryStage.show();
             ((Node) event.getSource()).getScene().getWindow().hide();
         }catch (IOException e){
-            System.err.println("Exception loading User Home view");
+            System.err.println("Exception loading User Home screen");
         }
     }
 
+    @FXML private Button LogoutButton; // takes user back to home essentially logging out since theres no way back
+    public void LogoutButtonClick(Event event){
+        HSC.HomeScene(LogoutButton);
+    }
 
 }
---
+++ src/model/usermodel/UserHomeModel.java
@@ -0,0 +1,37 @@
+package model.usermodel;
+
+import dao.EmployeeDAO;
+import dao.TableDAO;
+import model.LoginModel;
+
+public class UserHomeModel {
+    private String COVIDStatus;
+    EmployeeDAO EDAO = new EmployeeDAO();
+    TableDAO TDAO = new TableDAO();
+
+
+    public String getEmployeeName(){
+        EDAO.updateEmployee();
+        return EDAO.getEmployee(LoginModel.getCurrentUserID());
+    }
+
+    // shows current lockdown status
+    public String getCOVIDNotification(){
+        TDAO.updateTables();
+        if(TDAO.getCOVID(-1) == 0){ //table number -1 is used for "global" COVID status and messages by admin
+            COVIDStatus = "No lockdown";
+        } else if (TDAO.getCOVID(-1) == 1){
+            COVIDStatus = "Partial lockdown";
+        } else if (TDAO.getCOVID(-1) == 2){
+            COVIDStatus = "Total Lockdown";
+        }
+        return COVIDStatus;
+    }
+
+    // shows default admin message regarding COVID lockdown of the office
+    public String GetGlobalAdminMessage(){
+        TDAO.updateTables();
+        return TDAO.getAdminMessage(-1);
+    }
+
+}
