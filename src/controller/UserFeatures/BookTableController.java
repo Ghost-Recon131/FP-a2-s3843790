95eb5cbec065bd4d035cd7f4b611d2ca71d551be
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-21 23:17:40 +1000

    removed redundant methods

--- src/controller/UserFeatures/BookTableController.java
+++ src/controller/UserFeatures/BookTableController.java
@@ -1,14 +1,7 @@
 package controller.UserFeatures;
 
-import javafx.event.Event;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,20 +12,8 @@ public class BookTableController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
        //todo if necessary
     }
+    //todo Implement controller & model
 
-    // method for switching to Book Table view
-    public void BookTableScene(Event event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/UserView/BookTable.fxml"));
-            Parent root = loader.load();
-            Stage primaryStage = new Stage();
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-            ((Node) event.getSource()).getScene().getWindow().hide();
-        }catch (IOException e){
-            System.err.println("Exception loading Book table screen");
-        }
-    }
+
 
 }
--- src/controller/UserFeatures/UpdateBookingController.java
+++ src/controller/UserFeatures/UpdateBookingController.java
@@ -1,14 +1,7 @@
 package controller.UserFeatures;
 
-import javafx.event.Event;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,20 +12,8 @@ public class UpdateBookingController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         //todo if necessary
     }
+    //todo Implement controller & model
 
-    // method for switching to Update booking view
-    public void UpdateBookingScene(Event event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/UserView/UpdateBooking.fxml"));
-            Parent root = loader.load();
-            Stage primaryStage = new Stage();
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-            ((Node) event.getSource()).getScene().getWindow().hide();
-        }catch (IOException e){
-            System.err.println("Exception loading Update booking view");
-        }
-    }
+
 
 }
