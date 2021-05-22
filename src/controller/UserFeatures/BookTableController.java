94510c1beb45a154e75ee26bd7079788ae171b62
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-22 14:46:19 +1000

    progress on BookTable

--- src/controller/UserFeatures/BookTableController.java
+++ src/controller/UserFeatures/BookTableController.java
@@ -1,18 +1,30 @@
 package controller.UserFeatures;
 
+import controller.utils.ChangeSceneUtil;
+import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BookTableController implements Initializable {
-    UserHomeController UHC = new UserHomeController();
+    ChangeSceneUtil CSU = new ChangeSceneUtil();
 
     @Override // loads in some values without user interaction
     public void initialize(URL url, ResourceBundle rb) {
        //todo if necessary
     }
     //todo Implement controller & model
+    @FXML
+    public void setBackButtonClick(Event event){ // goes back to employee menu
+        CSU.ChangeScene(event,"/view/UserView/userHome.fxml");
+    }
+
+    @FXML
+    public void setLogoutButtonClick(Event event){ // goes back to Home Screen
+        CSU.ChangeScene(event,"/view/HomeScreen.fxml");
+    }
 
 
 
