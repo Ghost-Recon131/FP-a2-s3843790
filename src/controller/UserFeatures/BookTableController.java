e5fbd52089b324f0926dd968343a6c948cf37395
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-22 15:37:57 +1000

    Progress on Book table function

--- src/controller/UserFeatures/BookTableController.java
+++ src/controller/UserFeatures/BookTableController.java
@@ -4,11 +4,18 @@ import controller.utils.ChangeSceneUtil;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class BookTableController implements Initializable {
+    private LocalDate SelectedDate;
+    @FXML private Label DateError;
+    @FXML private Label TableError;
     ChangeSceneUtil CSU = new ChangeSceneUtil();
 
     @Override // loads in some values without user interaction
@@ -24,6 +31,13 @@ public class BookTableController implements Initializable {
     @FXML
     public void setLogoutButtonClick(Event event){ // goes back to Home Screen
         CSU.ChangeScene(event,"/view/HomeScreen.fxml");
+    }
+
+    @FXML private DatePicker Date; // get the date selected by the user
+    public void setDatePickerAction(){
+        SelectedDate = Date.getValue();
+        System.err.println("Selected date: " + SelectedDate);
+
     }
 
 
---
+++ src/controller/utils/DateCalculationUtil.java
@@ -0,0 +1,10 @@
+package controller.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DateCalculationUtil {
+    SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd");
+    Calendar calendar = Calendar.getInstance();
+
+}
