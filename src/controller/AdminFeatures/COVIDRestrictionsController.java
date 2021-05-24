d353bec31604d5d8e917232e06d604d67a632d69
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-24 22:19:38 +1000

    Added GUI elements

--- src/controller/AdminFeatures/COVIDRestrictionsController.java
+++ src/controller/AdminFeatures/COVIDRestrictionsController.java
@@ -3,9 +3,23 @@ package controller.AdminFeatures;
 import controller.utils.ChangeSceneUtil;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class COVIDRestrictionsController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class COVIDRestrictionsController implements Initializable {
+    @FXML private Label CustomMessageError;
+    @FXML private TextField COVIDStatus;
+
     ChangeSceneUtil CSU = new ChangeSceneUtil();
+
+    @Override // loads in some values as soon as user gets to home page
+    public void initialize(URL url, ResourceBundle rb) {
+        //todo get the current covid restriction status and display it
+    }
 
     @FXML
     public void setBackButtonClick(Event event){ // goes back to employee menu
@@ -16,6 +30,21 @@ public class COVIDRestrictionsController {
     public void setLogoutButtonClick(Event event){ // goes back to Home Screen
         CSU.ChangeScene(event,"/view/HomeScreen.fxml");
     }
-    //todo implement enacting COVID restrictions
 
+    @FXML
+    public void setNoRestriction(Event event){ // goes back to Home Screen
+        //todo
+    }
+
+    @FXML
+    public void setHalfCapacity(Event event){ // goes back to Home Screen
+        //todo
+    }
+
+    @FXML
+    public void setTotalLockdown(Event event){ // goes back to Home Screen
+        //todo
+    }
+
+    //todo implement enacting COVID restrictions
 }
