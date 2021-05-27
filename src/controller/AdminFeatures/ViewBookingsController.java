d897286f0a200a149f999b37072d5093ddd9d0ed
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-27 14:46:51 +1000

    progress on view booking feature

--- src/controller/AdminFeatures/ViewBookingsController.java
+++ src/controller/AdminFeatures/ViewBookingsController.java
@@ -7,7 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 import javafx.scene.shape.Rectangle;
 import model.AdminModel.ViewBookingsModel;
 
@@ -19,7 +19,7 @@ public class ViewBookingsController implements Initializable {
     private LocalDate SelectedDate = LocalDate.now();
     @FXML private Rectangle R1, R2, R3, R4, R5, R6, R7, R8, R9, R10;
     @FXML private Label PickDate;
-    @FXML private TextField Info1, Info2, Info3, Info4, Info5, Info6, Info7, Info8, Info9, Info10;
+    @FXML private TextArea Info1, Info2, Info3, Info4, Info5, Info6, Info7, Info8, Info9, Info10;
 
     ChangeSceneUtil CSU = new ChangeSceneUtil();
     TableStatusUtil TSU = new TableStatusUtil();
@@ -29,7 +29,7 @@ public class ViewBookingsController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         UpdateTables();
         PickDate.setText("Currently viewing bookings on : " + SelectedDate.toString());
-        DisplayBookingInformation();
+
     }
 
     @FXML
@@ -43,16 +43,16 @@ public class ViewBookingsController implements Initializable {
     }
 
     public void UpdateTables(){
-        TSU.SetTableColour(R1, 1, SelectedDate); // sets correct colours
-        TSU.SetTableColour(R2, 2, SelectedDate);
-        TSU.SetTableColour(R3, 3, SelectedDate);
-        TSU.SetTableColour(R4, 4, SelectedDate);
-        TSU.SetTableColour(R5, 5, SelectedDate);
-        TSU.SetTableColour(R6, 6, SelectedDate);
-        TSU.SetTableColour(R7, 7, SelectedDate);
-        TSU.SetTableColour(R8, 8, SelectedDate);
-        TSU.SetTableColour(R9, 9, SelectedDate);
-        TSU.SetTableColour(R10, 10, SelectedDate);
+        TSU.AdminTableView(R1, 1, SelectedDate, Info1); // sets correct colours
+        TSU.AdminTableView(R2, 2, SelectedDate, Info2);
+        TSU.AdminTableView(R3, 3, SelectedDate, Info3);
+        TSU.AdminTableView(R4, 4, SelectedDate, Info4);
+        TSU.AdminTableView(R5, 5, SelectedDate, Info5);
+        TSU.AdminTableView(R6, 6, SelectedDate, Info6);
+        TSU.AdminTableView(R7, 7, SelectedDate, Info7);
+        TSU.AdminTableView(R8, 8, SelectedDate, Info8);
+        TSU.AdminTableView(R9, 9, SelectedDate, Info9);
+        TSU.AdminTableView(R10, 10, SelectedDate, Info10);
     }
 
     @FXML private DatePicker Date; // get the date selected by the user
@@ -60,20 +60,7 @@ public class ViewBookingsController implements Initializable {
         SelectedDate = Date.getValue();
         PickDate.setText("Currently viewing bookings on : " + SelectedDate.toString());
         UpdateTables();
-        DisplayBookingInformation();
-    }
 
-    public void DisplayBookingInformation(){
-        VBM.GetBookingInfo(1, Info1, SelectedDate);
-        VBM.GetBookingInfo(2, Info2, SelectedDate);
-        VBM.GetBookingInfo(3, Info3, SelectedDate);
-        VBM.GetBookingInfo(4, Info4, SelectedDate);
-        VBM.GetBookingInfo(5, Info5, SelectedDate);
-        VBM.GetBookingInfo(6, Info6, SelectedDate);
-        VBM.GetBookingInfo(7, Info7, SelectedDate);
-        VBM.GetBookingInfo(8, Info8, SelectedDate);
-        VBM.GetBookingInfo(9, Info9, SelectedDate);
-        VBM.GetBookingInfo(10, Info10, SelectedDate);
     }
 
 }
--- src/model/AdminModel/ViewBookingsModel.java
+++ src/model/AdminModel/ViewBookingsModel.java
@@ -3,35 +3,12 @@ package model.AdminModel;
 import controller.utils.TableStatusUtil;
 import dao.BookingsDAO;
 import dao.EmployeeDAO;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 import java.time.LocalDate;
 
 public class ViewBookingsModel {
     BookingsDAO BDAO = new BookingsDAO();
     EmployeeDAO EDAO = new EmployeeDAO();
-    TableStatusUtil TSU = new TableStatusUtil();
 
-    public void GetBookingInfo(int TableNumber, TextField textfield, LocalDate Date){
-        BDAO.updateBookings();
-        EDAO.updateEmployee();
-
-        int BookingID = BDAO.getBookingIDUsingTableNumber(TableNumber);
-        if(BookingID == -1){
-            textfield.setText("Table " + TableNumber);
-        }else if(!BDAO.getTableAvailability(TableNumber, Date)){
-            int AccountID = BDAO.getAccountID(BookingID);
-            String EmployeeName = EDAO.getEmployee(AccountID);
-            String BookingStatus = BDAO.getBookingStatus(BookingID);
-            textfield.setText("Table " + TableNumber);
-            textfield.appendText("\n");
-            textfield.appendText(EmployeeName);
-            textfield.appendText("\n");
-            textfield.appendText("Booking Status: " + BookingStatus);
-            textfield.appendText("\n");
-            textfield.appendText("Booking ID: " + BookingID);
-            textfield.appendText("\n");
-            textfield.appendText("AccountID: " + AccountID);
-        }
-    }
 
 }
