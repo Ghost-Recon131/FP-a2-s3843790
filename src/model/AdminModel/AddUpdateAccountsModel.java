a947e86d5dc23369f7fa186532de2bf5a568d1bb
Jingxuan Feng <s3843790@student.rmit.edu.au>
2021-05-30 12:37:50 +1000

    Begin work on adding / update accounts

--- src/model/AdminModel/AddUpdateAccountsModel.java
+++ src/model/AdminModel/AddUpdateAccountsModel.java
@@ -1,5 +1,10 @@
 package model.AdminModel;
 
+import controller.utils.IntegerCheckUtil;
+import model.RegisterModel;
+
 public class AddUpdateAccountsModel {
+    RegisterModel registerModel = new RegisterModel();
+    IntegerCheckUtil ICU = new IntegerCheckUtil();
     //todo
 }
